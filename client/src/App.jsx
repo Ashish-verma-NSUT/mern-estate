e932089fb0cdeda77ca1051f0c357bd73b210397
Ashish Verma <vermaashish322@gmail.com>
2024-02-18 12:56:25 +0530

    complete signin page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ export default function App() {
   <BrowserRouter>
   <Header/>
   <Routes>
+    <Route path = "/" element={<Home/>}></Route>
     <Route path = "/home" element={<Home/>}></Route>
     <Route path="/sign-in" element={<SignIn/>}></Route>
     <Route path="/sign-up" element={<SignUp/>}></Route>
