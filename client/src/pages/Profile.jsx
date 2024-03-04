5c512ba0b49accf500d5433d643d7ec7356570fd
Ashish Verma <vermaashish322@gmail.com>
2024-03-04 15:57:00 +0530

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,7 +9,7 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import { FaDownload } from "react-icons/fa";
-import { deleteUserFailure, deleteUserStart, deleteUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice";
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -103,6 +103,21 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async ()=>{
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = res.json();
+      if(data.success === false){
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(data.message));
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -161,7 +176,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">{updateSuccess ? 'User is updated successfully!' : ''}</p>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -46,9 +46,21 @@ const userSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
         },
+        signOutUserStart: (state)=>{
+            state.loading = true;
+        },
+        signOutUserSuccess: (state) =>{
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        signOutUserFailure : (state, action)=>{
+            state.error = action.payload;
+            state.loading = false;
+        },
     }
 })
 
-export const {deleteUserFailure,deleteUserStart, deleteUserSuccess, signInFailure, signInStart, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess} = userSlice.actions;
+export const { signOutUserFailure, signOutUserStart, signOutUserSuccess, deleteUserFailure,deleteUserStart, deleteUserSuccess, signInFailure, signInStart, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess} = userSlice.actions;
 
 export default userSlice.reducer;
